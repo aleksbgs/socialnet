453d823d88f8c8c5ce6c072c54ff12aab637b8d4
Aleksandar Markovic <amarkovic@firstbeatmedia.com>
2019-12-27 21:44:01 +0100

    fix

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,7 +1,9 @@
 using API.Middleware;
 using Application.Activities;
+using Application.Interfaces;
 using Domain;
 using FluentValidation.AspNetCore;
+using Infrastructure.Security;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -48,6 +50,7 @@ namespace API
       identityBuiler.AddEntityFrameworkStores<DataContext>();
       identityBuiler.AddSignInManager<SignInManager<AppUser>>();
       services.AddAuthentication();
+      services.AddScoped<IJwtGenerator, JwtGenerator>();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
