792f682052de809b2f4f60bb92489f204e5c7856
Aleksandar Markovic <amarkovic@firstbeatmedia.com>
2019-12-28 23:42:56 +0100

    FIX

--- client-app/src/features/user/RegisterForm.tsx
+++ client-app/src/features/user/RegisterForm.tsx
@@ -10,33 +10,36 @@ import { combineValidators, isRequired } from 'revalidate'
 import ErrorMessage from '../../app/common/form/ErrorMessage'
 
 const validate = combineValidators({
+  username: isRequired('username'),
+  displayName: isRequired('display name'),
   email: isRequired('email'),
   password: isRequired('password')
 })
 
-const LoginForm = () => {
+const RegisterForm = () => {
 
   const rootStore = useContext(RootStoreContext);
-  const { login } = rootStore.userStore;
+  const { register } = rootStore.userStore;
 
   return (
     <FinalForm
-      onSubmit={(values: IUserFormValues) => login(values).catch(error => ({
+      onSubmit={(values: IUserFormValues) => register(values).catch(error => ({
         [FORM_ERROR]: error
       }))}
       validate={validate}
       render={({ handleSubmit, submitting, submitError, invalid, pristine, dirtySinceLastSubmit }) => (
         <Form onSubmit={handleSubmit} error>
-          <Header as='h2' content='Login to Reactivities' color='teal' textAlign='center' />
+          <Header as='h2' content='Sign up to Reactivities' color='teal' textAlign='center' />
+          <Field name='username' component={TextInput} placeholder='username' />
+          <Field name='displayName' component={TextInput} placeholder='Display Name' />
           <Field name='email' component={TextInput} placeholder='Email' />
           <Field name='password' component={TextInput} placeholder='Password' type='password' />
-          {submitError && !dirtySinceLastSubmit && (<ErrorMessage error={submitError} text='Invalid email or password' />)}
-          <Button disabled={(invalid && !dirtySinceLastSubmit) || pristine} loading={submitting} color='teal' content='Login' fluid />
-          {/* <pre>{JSON.stringify(form.getState(), null, 2)}</pre> */}
+          {(submitError && !dirtySinceLastSubmit) && (<ErrorMessage error={submitError} text='' />)}
+          <Button disabled={(invalid && !dirtySinceLastSubmit) || pristine} loading={submitting} color='teal' content='Register' fluid />
         </Form >
       )}
     />
   )
 }
 
-export default LoginForm
+export default RegisterForm
