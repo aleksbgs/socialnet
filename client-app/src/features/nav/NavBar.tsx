45d0c0ea0a010932fced8b4875d6a611061bb1f7
Aleksandar Markovic <amarkovic@firstbeatmedia.com>
2019-12-28 20:19:50 +0100

    fix

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,15 +1,15 @@
 import React, { useContext } from 'react'
-import { Menu, Container, Button } from 'semantic-ui-react'
+import { Menu, Container, Button, Image, Dropdown } from 'semantic-ui-react'
 import { observer } from 'mobx-react-lite';
 
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import { RootStoreContext } from '../../app/stores/rootStore';
 
 
 const NavBar: React.FC = () => {
 
   const rootStore = useContext(RootStoreContext);
-  const { openCreateForm } = rootStore.activityStore;
+  const { isLoggedIn, user } = rootStore.userStore;
 
   return (
     <Menu fixed='top' inverted>
@@ -19,8 +19,18 @@ const NavBar: React.FC = () => {
         </Menu.Item>
         <Menu.Item name='Activities' header as={NavLink} to='/activities' />
         <Menu.Item>
-          <Button as={NavLink} to='/createActivity' onClick={openCreateForm} positive content='Create Activity' />
+          <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
         </Menu.Item>
+        {user &&
+          <Menu.Item position='right'>
+            <Image avatar spaced='right' src={user.image || '/assets/user.png'} />
+            <Dropdown pointing='top left' text={user.displayName}>
+              <Dropdown.Menu>
+                <Dropdown.Item as={Link} to={`/profile/username`} text='My profile' icon='user' />
+                <Dropdown.Item text='Logout' icon='power' />
+              </Dropdown.Menu>
+            </Dropdown>
+          </Menu.Item>}
       </Container>
     </Menu>
 
